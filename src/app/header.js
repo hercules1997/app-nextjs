633fce26d4f1ca0390f4026720354aaf564ef330
hercules1997 <hercules.chaves.andrade@gmail.com>
2024-01-07 18:50:40 -0300

    update

---
+++ src/app/header.js
@@ -0,0 +1,8 @@
+
+export default function Header() {
+  return (
+    <header >
+      <h1>App NextJS</h1>
+    </header>
+  );
+}
---
+++ src/app/main.js
@@ -0,0 +1,8 @@
+
+export default function Main() {
+  return (
+    <main >
+      <h2>Main</h2>
+    </main>
+  );
+}
